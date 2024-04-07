30553c9e8b14659ca902e3bf06b920d016af8404
Usman Shahzad <usman.shahzad@apparound.com>
2024-04-07 06:32:11 +0200

    Create Pokedex project

---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.DM;
+using Pokedex.Services;
+
+namespace Pokedex.Controllers
+{
+    [Route("api/pokemon")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("{pokemonName}")]
+        public IActionResult GetPokemonInfo(string pokemonName)
+        {
+            PokemonService _pokemonService = new PokemonService(new HttpClient());
+
+            Pokemon pokemon = _pokemonService.GetPokemonInfo(pokemonName);
+
+            if (!String.IsNullOrEmpty(pokemon.Name))
+            {
+                return Ok(pokemon);
+            }
+            else
+            {
+                return BadRequest("Pokemon not found"); 
+            }
+        }
+    }
+}
---
+++ Pokedex/Services/PokemonService.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Pokedex.DM;
+
+namespace Pokedex.Services
+{
+    public class PokemonService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokemonService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Pokemon GetPokemonInfo(string pokemonName)
+        {
+            _httpClient.BaseAddress = new Uri($"https://pokeapi.co/api/v2/pokemon-species/{pokemonName.ToLower()}/");
+
+            var response = _httpClient.GetAsync(_httpClient.BaseAddress).Result;
+
+            try
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                    var dynamicObject = JsonConvert.DeserializeObject<dynamic>(responseContent)!;
+
+                    var pokemon = new Pokemon()
+                    {
+                        Name = dynamicObject.name,
+                        Description = dynamicObject.flavor_text_entries[0].flavor_text,
+                        Habitat = dynamicObject.habitat.name,
+                        IsLegendary = dynamicObject.is_legendary
+                    };
+
+                    return pokemon;
+                }
+                else
+                {
+                    Console.WriteLine("Error: " + response.StatusCode);
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return new Pokemon();
+        }
+    }
+}
