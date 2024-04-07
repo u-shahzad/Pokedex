58afa48afb604a393f0ce67726566af67a9b8173
Usman Shahzad <usman.shahzad@apparound.com>
2024-04-08 00:58:16 +0200

    Add Dockerfile

--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -7,7 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks();
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
